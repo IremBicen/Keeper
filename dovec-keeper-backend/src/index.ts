f3a6fe86f907c16eb9986388e5dd1d5865ebdd9f
IremBicen <iremsubicen1@gmail.com>
2025-12-10 09:58:25 +0200

    Latest update

--- dovec-keeper-backend/src/index.ts
+++ dovec-keeper-backend/src/index.ts
@@ -14,11 +14,14 @@ dotenv.config();
 const app = express();
 
 const allowedOrigins = [
-  "http://localhost:3000"
+  "http://localhost:3000",
+  "http://13.51.140.18",
 ];
 
 app.use(cors({
   origin: allowedOrigins,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 }));
 
